90ebfc812e6ff526c3f64c0178a4d7cfa7a5083f
ishimwent <c.ishimwent@alustudent.com>
2025-06-02 12:51:51 +0000

    hello

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module to generate Pascal's Triangle.
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal’s triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        # Compute the next row with 1 at both ends and sum of pairs in between
+        row = [1]
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
