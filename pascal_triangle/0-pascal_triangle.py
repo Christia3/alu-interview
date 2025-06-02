ece237447a6b82a11f90a71859f471eabbccef62
ishimwent <c.ishimwent@alustudent.com>
2025-06-02 12:54:41 +0000

    hello

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,9 +3,16 @@
 Module to generate Pascal's Triangle.
 """
 
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing Pascal’s triangle of n.
+
+    Args:
+        n (int): Number of rows in the triangle.
+
+    Returns:
+        List[List[int]]: Pascal's triangle represented as a list of lists.
     """
     if n <= 0:
         return []
@@ -14,7 +21,6 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        # Compute the next row with 1 at both ends and sum of pairs in between
         row = [1]
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
